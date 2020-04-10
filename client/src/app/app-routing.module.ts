e37d06ccc92f1c35115a790518bbee07a1777117
Shawn Reuter <reute078@morris.umn.edu>
2020-04-09 20:58:50 -0500

    Remove routing to home component
    Removed routing to home component and removed sidebar tab. Progresses #15

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AddNoteComponent } from './notes/add-note.component';
 import { OwnerListComponent } from './owners/owner-list.component';
+import { OwnerCardComponent } from './owners/owner-card.component';
 import { OwnerDoorBoardComponent } from './owners/owner-doorboard.component';
 import { AddOwnerComponent } from './owners/add-owner.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: OwnerListComponent},
   {path: 'owner/:id/notes/new', component: AddNoteComponent},
   {path: 'owners', component: OwnerListComponent},
   {path: 'owners/new', component: AddOwnerComponent},
